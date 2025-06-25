ed0e246d494f0936ad931428b915781906be6c29
renatogomesf <renato_gfe@hotmail.com>
2025-06-25 16:17:19 -0300

    criado cards de info

---
+++ src/components/BalanceItem/index.tsx
@@ -0,0 +1,35 @@
+import { useMemo } from "react";
+
+import { View, Text } from "react-native";
+
+export default function BalanceItem({ data }: any) {
+  const labelName = useMemo(() => {
+    if (data.tag === "saldo") {
+      return {
+        label: "Saldo atual",
+        color: "bg-[#3b3dbf]",
+      };
+    } else if (data.tag === "receita") {
+      return {
+        label: "Entradas de Hoje",
+        color: "bg-[#00b94a]",
+      };
+    } else {
+      return {
+        label: "Saídas de Hoje",
+        color: "bg-[#ef463a]",
+      };
+    }
+  }, []);
+
+  return (
+    <View
+      className={`mx-[14px] rounded-[4px] justify-center items-start w-[300px] h-[200px] pl-[14px] ${labelName.color}`}
+    >
+      <Text className="text-[#fff] text-[19px] font-bold">
+        {labelName.label}
+      </Text>
+      <Text className="mt-[5px] text-[#fff] text-[30px]">R$ {data.saldo}</Text>
+    </View>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,72 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
-import { View, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
 
 import { AuthContext } from "../../contexts/auth";
 
 import Header from "../../components/Header";
 
+import api from "../../services/api";
+
+import { format } from "date-fns";
+
+// verifica se você está na tela. se sim retorna true... se não, retorna false
+import { useIsFocused } from "@react-navigation/native";
+
+import BalanceItem from "../../components/BalanceItem";
+
 export default function Home() {
-  const { signOut, user } = useContext(AuthContext);
+  // const { signOut, user } = useContext(AuthContext);
+
+  const isFocused = useIsFocused();
+
+  const [listBalance, setListBalance] = useState([]);
+
+  const [dateMovements, setDateMovements] = useState(new Date());
+
+  useEffect(() => {
+    let isActive = true;
+
+    async function getMovements() {
+      let dateFormated = format(dateMovements, "dd/MM/yyyy");
+
+      const balance = await api.get("/balance", {
+        params: {
+          date: dateFormated,
+        },
+      });
+
+      if (isActive) {
+        setListBalance(balance.data);
+      }
+    }
+
+    getMovements();
+
+    // chamado quando o componente é desmontado
+    return () => {
+      isActive = false;
+    };
+  }, [isFocused]);
 
   return (
     // SafeAreaView: garante que o conteúdo não fique por detrás das câmeras caso o aparelho possua a "gota" que invade a tela.
     <SafeAreaView className="flex-1 bg-[#f0f4ff]">
       <Header title={"Minhas movimentações"} />
+
+      <FlatList
+        data={listBalance}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item: any) => item.tag}
+        renderItem={({ item }) => <BalanceItem data={item} />}
+      />
     </SafeAreaView>
   );
 }
